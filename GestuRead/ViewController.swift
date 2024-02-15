86773aae36e18c77bce8338c0ccca8d7c176b3e1
Jose Antonio <jcordon5@usal.es>
2024-02-15 17:18:32 +0100

    Initial Commit

---
+++ GestuRead/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GestuRead
+//
+//  Created by Jose Antonio Cordon Muñoz on 15/2/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
